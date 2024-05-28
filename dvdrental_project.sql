2869ee6b7b5169cf6f97b1cdb179d9bf9ea544a6
Kamruzzamansust <kamruzzaman.sust15@gmail.com>
2024-05-28 14:50:42 +0600

    updated

--- dvdrental_project.sql
+++ dvdrental_project.sql
@@ -13,6 +13,66 @@ FROM actor
 GROUP BY first_name, last_name
 HAVING COUNT(*) > 1;
 
---
+--Display the customer names that share the same address (e.g. husband and wife).
+
+SELECT c1.first_name, c1.last_name, c2.first_name, c2.last_name
+FROM customer c1
+JOIN customer c2 
+ON c1.customer_id <> c2.customer_id 
+AND c1.address_id = c2.address_id;
+
+--Display the total amount payed by all customers in the payment table.
+
+SELECT SUM(amount)
+From Payment;
+
+-- Display the total amount payed by each customer in the payment table.
+SELECT customer_id , SUM(amount)
+FROM payment
+GROUP BY customer_id
+ORDER BY customer_id;
+
+-- What is the highest total_payment done.
+SELECT SUM(amount) AS total_payments
+FROM payment
+GROUP BY customer_id 
+ORDER BY SUM(amount) DESC LIMIT 1 ;
+
+
+--What is the name of the customer who made the highest total payments.
+
+SELECT customer_id, first_name ,last_name 
+FROM customer
+WHERE customer_id IN (SELECT customer_id 
+									FROM payment
+									GROUP BY customer_id
+									HAVING SUM(amount) = 
+											(SELECT SUM(amount) AS total_payments
+											FROM payment
+											GROUP BY customer_id 
+											ORDER BY SUM(amount) DESC LIMIT 1));
+
+-- What is the movie(s) that was rented the most.
+SELECT film_id ,title
+FROM film WHERE film_id IN(
+					SELECT film_id 
+					FROM rental AS R JOIN inventory AS I 
+					ON R.inventory_id = I.inventory_id 
+					GROUP BY film_id 
+					ORDER BY COUNT(film_id) DESC LIMIT 1) ;
+
+
+
+
+---8. Which movies have been rented so far.
+
+SELECT COUNT(title)
+FROM film 
+WHERE film_id IN(
+	
+	SELECT DISTINCT(film_id) 
+	FROM inventory AS I 
+	JOIN rental AS R 
+	ON I.inventory_id = R.inventory_id);
 
 
